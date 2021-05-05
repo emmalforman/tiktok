1764e20661b4d524e10c863b254823693eac72e6
emmalforman <73502597+emmalforman@users.noreply.github.com>
2021-05-05 15:30:43 -0700

    playing around with items in dictionary

--- app/trending.py
+++ app/trending.py
@@ -8,17 +8,29 @@ results = 10
 
 # Since TikTok changed their API you need to use the custom_verifyFp option. 
 # In your web browser you will need to go to TikTok, Log in and get the s_v_web_id value.
-trending = api.trending(count=10, custom_verifyFp="")
+trending = api.trending(count=4, custom_verifyFp="")
 '''
 for tiktok in trending:
     # Prints the hashtag info of trending tiktoks
     for d in tiktok["textExtra"]:
         hashtags.append(d["hashtagName"])
     print(hashtags)
-
 '''
 for tiktok in trending:
     # Prints the text of the tiktok
-    print(tiktok["textExtra"])
+    print(type(tiktok))
+    tag = False
+    print(tiktok)
+    for d in tiktok:  
+       # print(d)
+        '''
+        info = list(d.keys())
+        if "textExtra" in info:
+            tag = True
+    if tag == True:
+        print(tiktok[9]['hashtagName'])
+    else:
+        print("no hashtag")
+        '''
 
 print(len(trending))
