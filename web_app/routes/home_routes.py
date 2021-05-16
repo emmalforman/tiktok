199ccc437ff8c590398a218fd2b36f6323070822
emmalforman <73502597+emmalforman@users.noreply.github.com>
2021-05-16 11:46:14 -0400

    get web app running on Flask

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,27 +1,26 @@
-from flask import Flask, request, jsonify
+# web_app/routes/home_routes.py
 
-app = Flask(__name__)
+from flask import Blueprint, request, render_template
 
-#
-# HOME ROUTES
-#
+home_routes = Blueprint("home_routes", __name__)
 
-@app.route("/")
-@app.route("/home")
+@home_routes.route("/")
+@home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
+    return render_template("home.html")
 
-@app.route("/about")
+@home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
+    return render_template("about.html")
 
-@app.route("/hello")
+@home_routes.route("/hello")
 def hello_world():
     print("HELLO...", dict(request.args))
     # NOTE: `request.args` is dict-like, so below we're using the dictionary's `get()` method,
     # ... which will return None instead of throwing an error if key is not present
     # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
     name = request.args.get("name") or "World"
-    return f"Hello, {name}!"
+    message = f"Hello, {name}!"
+    return render_template("hello.html", message=message)
